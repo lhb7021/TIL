375a869576edf2e054f3ca83b127661836e956e4
lhb7021 <manevil6@gmail.com>
2021-07-31 19:52:31 +0900

    lean:ts:type inference

--- typescript/learn/app.js
+++ typescript/learn/app.js
@@ -1,5 +1,15 @@
-// Get start
-function logName(name) {
-    console.log(name);
+// Type inference
+var a = 5;
+a = 10;
+var student = {
+    name: 'Jake',
+    course: 'Getting Started with TypeScript',
+    codingIQ: 80,
+    code: function () {
+        console.log('brain is working hard');
+    }
+};
+student.name = 100;
+function calculateCodingIQ(lostPoints) {
+    return 100 - lostPoints;
 }
-logName('jack');
--- typescript/learn/app.ts
+++ typescript/learn/app.ts
@@ -1,6 +1,19 @@
-// Get start
-function logName (name: string) {
-  console.log(name);
-}
+// Type inference
+let a = 5;
+a = 10;
 
-logName('jack');
+let student = {
+  name: 'Jake',
+  course: 'Getting Started with TypeScript',
+  codingIQ: 80,
+  code: function () {
+    console.log('brain is working hard');
+  }
+};
+
+student.name = 100;
+
+
+function calculateCodingIQ (lostPoints) {
+  return 100 - lostPoints;
+}
