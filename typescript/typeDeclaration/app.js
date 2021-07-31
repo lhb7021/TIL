8f22cb339fbac82058decda0a9945b6f9bece9ba
lhb7021 <manevil6@gmail.com>
2021-07-31 23:47:07 +0900

    lean:ts:typeDeclaration

---
+++ typescript/typeDeclaration/app.js
@@ -0,0 +1,9 @@
+var studentID = 12345;
+var studentName = 'Hanbin Lee';
+var age = 21;
+var gender = 'male';
+var subject = 'Javascript';
+var courseCompleted = false;
+function getStudentDetails(studentID) {
+    return null;
+}
---
+++ typescript/typeDeclaration/app.ts
@@ -0,0 +1,17 @@
+let studentID: number = 12345;
+let studentName: string = 'Hanbin Lee';
+let age: number = 21;
+let gender: string = 'male';
+let subject: string = 'Javascript';
+let courseCompleted: boolean = false;
+
+function getStudentDetails (studentID: number): {
+  studentID: number;
+  studentName: string;
+  age: number;
+  gender: string;
+  subject: string;
+  createdDate: Date;
+} {
+  return null;
+}
