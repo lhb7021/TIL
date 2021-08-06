f3a9a6c30e2bec8d60d78bc4953e481fd0935103
lhb7021 <manevil6@gmail.com>
2021-08-06 18:52:46 +0900

    lean:ts:accessModifiers

--- typescript/tsOOP/app.js
+++ typescript/tsOOP/app.js
@@ -7,25 +7,55 @@
 // : void => {
 //   console.log(`${fullName}의 직업은 ${jobTitle} 이고 일주일의 수입은 ${hourlyRate * workingHoursPerWeek} 달러 이다.`);
 // };
+// class Employee {
+//   fullName: string;
+//   age: number;
+//   jobTitle = '주니어 개발자';
+//   hourlyRate: number;
+//   workingHoursPerWeek: number;
+//   printEmployeeDatails = (): void => {
+//     console.log(`${this.fullName}의 직업은 ${this.jobTitle} 이고 일주일의 수입은 ${this.hourlyRate * this.workingHoursPerWeek} 달러 이다.`);
+//   }
+// }
+// let employee1 = new Employee();
+// employee1.fullName = 'Hanbin Lee';
+// employee1.age = 21;
+// employee1.hourlyRate = 40;
+// employee1.workingHoursPerWeek = 35;
+// employee1.printEmployeeDatails();
+// let employee2 = new Employee();
+// employee2.fullName = 'Coding Kim';
+// employee2.age = 23;
+// employee2.hourlyRate = 40;
+// employee2.workingHoursPerWeek = 40;
+// employee2.printEmployeeDatails();
 var Employee = /** @class */ (function () {
-    function Employee() {
+    function Employee(_fullName, _age, _hourlyRate, workingHoursPerWeek, jobTitle) {
         var _this = this;
-        this.jobTitle = '주니어 개발자';
+        if (jobTitle === void 0) { jobTitle = '주니어 개발자'; }
+        this._fullName = _fullName;
+        this._age = _age;
+        this._hourlyRate = _hourlyRate;
+        this.workingHoursPerWeek = workingHoursPerWeek;
+        this.jobTitle = jobTitle;
         this.printEmployeeDatails = function () {
-            console.log(_this.fullName + "\uC758 \uC9C1\uC5C5\uC740 " + _this.jobTitle + " \uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 " + _this.hourlyRate * _this.workingHoursPerWeek + " \uB2EC\uB7EC \uC774\uB2E4.");
+            console.log(_this._fullName + "\uC758 \uC9C1\uC5C5\uC740 " + _this.jobTitle + " \uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 " + _this._hourlyRate * _this.workingHoursPerWeek + " \uB2EC\uB7EC \uC774\uB2E4.");
         };
     }
+    Object.defineProperty(Employee.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (value) {
+            this._fullName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
-var employee1 = new Employee();
-employee1.fullName = 'Hanbin Lee';
-employee1.age = 21;
-employee1.hourlyRate = 40;
-employee1.workingHoursPerWeek = 35;
+var employee1 = new Employee('Hanbin Lee', 21, 40, 35);
+console.log(employee1.fullName);
+employee1.fullName = 'Coding Kim';
+console.log(employee1.fullName);
 employee1.printEmployeeDatails();
-var employee2 = new Employee();
-employee2.fullName = 'Coding Kim';
-employee2.age = 23;
-employee2.hourlyRate = 40;
-employee2.workingHoursPerWeek = 40;
-employee2.printEmployeeDatails();
--- typescript/tsOOP/app.ts
+++ typescript/tsOOP/app.ts
@@ -9,28 +9,57 @@
 //   console.log(`${fullName}의 직업은 ${jobTitle} 이고 일주일의 수입은 ${hourlyRate * workingHoursPerWeek} 달러 이다.`);
 // };
 
+// class Employee {
+//   fullName: string;
+//   age: number;
+//   jobTitle = '주니어 개발자';
+//   hourlyRate: number;
+//   workingHoursPerWeek: number;
+
+//   printEmployeeDatails = (): void => {
+//     console.log(`${this.fullName}의 직업은 ${this.jobTitle} 이고 일주일의 수입은 ${this.hourlyRate * this.workingHoursPerWeek} 달러 이다.`);
+//   }
+// }
+
+// let employee1 = new Employee();
+// employee1.fullName = 'Hanbin Lee';
+// employee1.age = 21;
+// employee1.hourlyRate = 40;
+// employee1.workingHoursPerWeek = 35;
+// employee1.printEmployeeDatails();
+
+// let employee2 = new Employee();
+// employee2.fullName = 'Coding Kim';
+// employee2.age = 23;
+// employee2.hourlyRate = 40;
+// employee2.workingHoursPerWeek = 40;
+// employee2.printEmployeeDatails();
+
 class Employee {
-  fullName: string;
-  age: number;
-  jobTitle = '주니어 개발자';
-  hourlyRate: number;
-  workingHoursPerWeek: number;
+  constructor(
+    private _fullName: string, 
+    private _age: number, 
+    private _hourlyRate: number, 
+    public workingHoursPerWeek: number,
+    public jobTitle = '주니어 개발자'){
+  }
+
+  get fullName () {
+    return this._fullName;
+  }
+
+  set fullName (value: string) {
+    this._fullName = value;
+  }
 
   printEmployeeDatails = (): void => {
-    console.log(`${this.fullName}의 직업은 ${this.jobTitle} 이고 일주일의 수입은 ${this.hourlyRate * this.workingHoursPerWeek} 달러 이다.`);
+    console.log(`${this._fullName}의 직업은 ${this.jobTitle} 이고 일주일의 수입은 ${this._hourlyRate * this.workingHoursPerWeek} 달러 이다.`);
   }
 }
 
-let employee1 = new Employee();
-employee1.fullName = 'Hanbin Lee';
-employee1.age = 21;
-employee1.hourlyRate = 40;
-employee1.workingHoursPerWeek = 35;
-employee1.printEmployeeDatails();
+let employee1 = new Employee('Hanbin Lee', 21, 40, 35);
+console.log(employee1.fullName);
+employee1.fullName = 'Coding Kim';
+console.log(employee1.fullName);
 
-let employee2 = new Employee();
-employee2.fullName = 'Coding Kim';
-employee2.age = 23;
-employee2.hourlyRate = 40;
-employee2.workingHoursPerWeek = 40;
-employee2.printEmployeeDatails();
+employee1.printEmployeeDatails();
