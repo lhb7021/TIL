3c602c831f4fa87e0af4712816b95a2fa54a4151
lhb7021 <manevil6@gmail.com>
2021-11-15 18:31:20 +0900

    learn:python

---
+++ python/practice.py
@@ -0,0 +1,81 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*(3+1))
+
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋ")
+# print("ㅋ"*9)
+
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not (5 > 10))
+
+'''
+# 애완동물을 소개해 주세요~
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "낮잠"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "예요")
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name,"는", age, "살이며, ", hobby, "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+'''
+
+# print(1+1) # 2
+# print(3-2) # 1
+# print(5*2) # 10
+# print(6/3) # 2.0
+
+# print(2**3) # 8
+# print(5%3) # 2
+# print(10%3) # 1
+# print(5//3) # 1
+# print(10//3) # 3
+
+# print(10 > 3) # True
+# print(4 >= 7) # False
+
+# print(3 == 3) # True
+# print(4 == 3) # False
+# print(3 + 4 == 7) # True
+
+# print(1 != 3) # True
+# print(not(1 != 3)) # False
+
+# print((3 > 0) and (3 < 5)) # True
+# print((3 > 0) & (3 < 5)) # True
+
+# print((3 > 0) or (3 > 5)) # True
+# print((3 > 0) | (3 > 5)) # True
+
+# print(5 > 4 > 3) # True
+# print(5 > 4 > 7) # False
+
+# number = 2 + 3 * 4 # 14
+# print(number)
+# number += 4 # 18
+# print(number)
+# number *= 2 # 36
+# print(number)
+# number /= 2 # 18
+# print(number)
+# number -= 2 # 16
+# print(number)
+# number %= 2 # 0
+# print(number)
+
+print(abs(-5)) # 5
+print(pow(4, 2)) # 4^2 = 16
+print(max(5, 12)) # 12
+print(min(5, 12)) # 5
