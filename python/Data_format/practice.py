8acd5dbace7b92a2d8b75d58a194ddc80dfb5617
lhb7021 <manevil6@gmail.com>
2021-11-18 14:32:15 +0900

    learn:python:data_format

---
+++ python/Data_format/practice.py
@@ -0,0 +1,23 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*(3+1))
+
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+name = "Keuu"
+comment = "Hello!"
+print(name + ", " + comment)
