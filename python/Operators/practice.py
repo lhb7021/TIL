4935d12da33182b69d8fa6296af119be750a04b4
lhb7021 <manevil6@gmail.com>
2021-11-18 14:37:00 +0900

    learn:python:operators

---
+++ python/Operators/practice.py
@@ -0,0 +1,46 @@
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/3) # 2.0
+
+print(2**3) # 8
+print(5%3) # 2
+print(10%3) # 1
+print(5//3) # 1
+print(10//3) # 3
+
+print(10 > 3) # True
+print(4 >= 7) # False
+
+print(3 == 3) # True
+print(4 == 3) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
+
+number = 2 + 3 * 4
+print(number) # 14
+number += 4
+print(number) # 18
+number *= 2
+print(number) # 36
+number /= 2
+print(number) # 18.0
+number -= 2 
+print(number) # 16.0
+number %= 6 
+print(number) # 4.0
+number **= 2
+print(number) # 16.0
+number //= 4
+print(number) # 4.0
