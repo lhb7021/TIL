f9ce681b5d7f2a92c2d7caf8581887b7d0d4cf72
lhb7021 <manevil6@gmail.com>
2021-11-18 16:56:07 +0900

    learn:python:escape_codes

---
+++ python/Escape_codes/practice.py
@@ -0,0 +1,10 @@
+print("백문이 불여일견\n백견이 불여일타")
+
+print('저는 "Keuu"입니다.')
+print("저는 \"Keuu\"입니다.")
+
+print("Red Apple\rPine") # \r : 커서를 맨 앞으로 이동
+
+print("Redd\bApple") # \b : 백스페이스 (한 글자 삭제)
+
+print("Red\tApple")
