016dcc2723befaa1446deaafa5db57cd027df6aa
lhb7021 <manevil6@gmail.com>
2021-11-19 14:35:52 +0900

    learn:python:standard_in_out_put

---
+++ python/Standard_in_out_put/practice.py
@@ -0,0 +1,17 @@
+import sys
+print("Python", "Java", file=sys.stdout)
+print("Python", "Java", file=sys.stderr)
+
+scores = {"수학":0, "영어":50, "코딩":100}
+for subject, score in scores.items():
+  # print(subject, score)
+  print(subject.ljust(8), str(score).rjust(4), sep=":")
+
+for num in range(1, 21):
+  print("대기번호 : " + str(num).zfill(3))
+
+answer = input("아무 값이나 입력하세요 : ")
+print("입력하신 값은 " + answer + "입니다")
+print(type(answer))
+answer = 10
+print(type(answer))
