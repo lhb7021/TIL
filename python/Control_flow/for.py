607bec8ebeab5d08f2c6f8d72c454290985781e0
lhb7021 <manevil6@gmail.com>
2021-11-19 12:56:22 +0900

    learn:python:for

---
+++ python/Control_flow/for.py
@@ -0,0 +1,24 @@
+print("대기번호 : 1")
+print("대기번호 : 2")
+print("대기번호 : 3")
+print("대기번호 : 4")
+
+for waiting_no in range(1, 6):
+  print("대기번호 : {0}".format(waiting_no))
+
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+  print("{0}, 커피가 준비되었습니다.".format(customer))
+
+students = [1,2,3,4,5]
+print(students)
+students = [i+100 for i in students]
+print(students)
+
+students = ["Iron man", "Thor", "I am groot"]
+students = [len(i) for i in students]
+print(students)
+
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
